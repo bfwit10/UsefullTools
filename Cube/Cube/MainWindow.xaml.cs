e08259ae08f7be6e4e1e41a4816a3313cb3565b8
CreativeRyu <Creative.Ryu@web.de>
2019-02-12 12:09:39 +0100

    v2.1
    Rotation ist integriert

--- Cube/Cube/MainWindow.xaml.cs
+++ Cube/Cube/MainWindow.xaml.cs
@@ -25,16 +25,31 @@ namespace Cube
             InitializeComponent();
         }
 
-        private void Scollbar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {           
-            angleContent_x.Content = "X - Achse: " + Math.Floor(vScroll.Value).ToString() + "°";
-            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector(1, 0, 0), vScroll.Value));
+        public void Viewport3D_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Viewport3D viewPort = sender as Viewport3D;
+            Point mousePosition = e.GetPosition(viewPort);
+            HitTestResult result = VisualTreeHelper.HitTest(viewPort, mousePosition);
+            if (result.VisualHit.GetType() == typeof(ModelVisual3D))
+            {
+                ModelVisual3D v3D = result.VisualHit as ModelVisual3D;
+                RotateTransform3D rot = (v3D.Transform as RotateTransform3D);
+                (rot.Rotation as AxisAngleRotation3D).Angle += 10;
+            }
+
         }
 
-        private void Scollbar_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void Scrollbar_X_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            angleContent_y = "Y - Achse: " + Math.Floor(hScroll.Value).ToString() + "°";
-            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), hScroll.Value));
+            angleContent_x.Content = "X - Achse: " + Math.Floor(Scrollbar_X.Value).ToString() + "°";
+            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), Scrollbar_X.Value));
         }
+
+        private void Scrollbar_Y_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            angleContent_y.Content = "Y - Achse: " + Math.Floor(Scrollbar_Y.Value).ToString() + "°";
+            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), Scrollbar_Y.Value));
+        }
+
     }
 }
