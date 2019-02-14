ebb0793803d59a368767db6e06935cc672dc1fd9
CreativeRyu <Creative.Ryu@web.de>
2019-02-14 15:10:28 +0100

    v2.2
    Rotation erweitert

--- Cube/Cube/MainWindow.xaml.cs
+++ Cube/Cube/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -25,31 +26,83 @@ namespace Cube
             InitializeComponent();
         }
 
-        public void Viewport3D_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            Viewport3D viewPort = sender as Viewport3D;
-            Point mousePosition = e.GetPosition(viewPort);
-            HitTestResult result = VisualTreeHelper.HitTest(viewPort, mousePosition);
-            if (result.VisualHit.GetType() == typeof(ModelVisual3D))
-            {
-                ModelVisual3D v3D = result.VisualHit as ModelVisual3D;
-                RotateTransform3D rot = (v3D.Transform as RotateTransform3D);
-                (rot.Rotation as AxisAngleRotation3D).Angle += 10;
-            }
-
-        }
-
-        private void Scrollbar_X_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        public void Scrollbar_X_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             angleContent_x.Content = "X - Achse: " + Math.Floor(Scrollbar_X.Value).ToString() + "°";
             Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), Scrollbar_X.Value));
         }
 
-        private void Scrollbar_Y_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        public void Scrollbar_Y_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             angleContent_y.Content = "Y - Achse: " + Math.Floor(Scrollbar_Y.Value).ToString() + "°";
-            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), Scrollbar_Y.Value));
+            Cube.Transform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0,1, 0), Scrollbar_Y.Value));
         }
 
+        private void Viewport3D_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+            if ((Keyboard.Modifiers == ModifierKeys.Alt))
+            {
+
+                if (e.Delta > 0)
+                {
+                    Viewport3D viewPort = sender as Viewport3D;
+                    Point mousePosition = e.GetPosition(viewPort);
+                    HitTestResult result = VisualTreeHelper.HitTest(viewPort, mousePosition);
+                    ModelVisual3D vYPlus3D = result.VisualHit as ModelVisual3D;
+                    RotateTransform3D rotYPlus = (vYPlus3D.Transform as RotateTransform3D);
+                    (rotYPlus.Rotation as AxisAngleRotation3D).Angle += 10;
+                }
+
+                else if (e.Delta < 0)
+                {
+                    Viewport3D viewPort = sender as Viewport3D;
+                    Point mousePosition = e.GetPosition(viewPort);
+                    HitTestResult result = VisualTreeHelper.HitTest(viewPort, mousePosition);
+                    ModelVisual3D vYMinus3D = result.VisualHit as ModelVisual3D;
+                    RotateTransform3D rotYMinus = (vYMinus3D.Transform as RotateTransform3D);
+                    (rotYMinus.Rotation as AxisAngleRotation3D).Angle -= 10;
+                }
+            }
+            else 
+            {
+                
+                if (e.Delta > 0)
+                {
+                    Scrollbar_X.Value = Scrollbar_X.Value + 10;
+                }
+
+                else if (e.Delta < 0)
+                {
+
+                    Scrollbar_X.Value = Scrollbar_X.Value - 10;
+
+                }
+            }           
+        }
+
+
+        // Methode zur Skalierung des Würfels
+        //private void Viewport3D_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        //{
+        //    if (Keyboard.Modifiers != ModifierKeys.Control)
+        //        return;
+
+        //    if (e.Delta > 0)
+        //    {
+        //        zoom = new ScaleTransform3D();
+        //        ScaleTransform3D.ScaleY + 0.2;
+        //        ScaleTransform3D.ScaleY + 0.2;
+        //    }
+        //    //PerspectiveCamera.Position()
+
+        //    else if (e.Delta < 0)
+        //    {
+        //        ScaleTransform3D.ScaleXProperty - 0.2;
+        //        ScaleTransform3D.ScaleYProperty - 0.2;
+        //        ScaleTransform3D.ScaleYProperty - 0.2;
+        //    }
+
+        //}
     }
 }
